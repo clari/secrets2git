866f1c64ca8a14611075a88697598e425fa78872
crizCraig <cquiter@gmail.com>
2015-12-17 17:12:51 -0800

    Clarify output

--- secrets2git.py
+++ secrets2git.py
@@ -7,19 +7,32 @@ import os
 from cryptography.fernet import Fernet
 import imp
 import sys
+import subprocess
 
 CONF_FILE_NAME = 'Secrets2GitConf.py'
 PARENT_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + '/'
 conf = imp.load_source('conf', PARENT_DIR + CONF_FILE_NAME)
 EXTENSION = '.encrypted'
 
+
+def say(message):
+    print('secrets2git: ' + message)
+
+if not conf.AWS_ACCESS_KEY_ID:
+    say('AWS_ACCESS_KEY_ID must be set in your environment')
+    exit(1)
+
+if not conf.AWS_SECRET_ACCESS_KEY:
+    say('AWS_SECRET_ACCESS_KEY must be set in your environment')
+    exit(1)
+
 client = boto3.client('kms', region_name=conf.REGION_NAME,
                       aws_access_key_id=conf.AWS_ACCESS_KEY_ID,
                       aws_secret_access_key=conf.AWS_SECRET_ACCESS_KEY)
 
 if 'KEY' not in dir(conf):
-    print('secrets2git key not found in ' + CONF_FILE_NAME)
-    print('Do you want to create a new one?')
+    say('secrets2git key not found in ' + CONF_FILE_NAME)
+    say('Do you want to create a new one?')
     answer = raw_input()
     if 'y' in answer:
         key = Fernet.generate_key()
@@ -27,15 +40,15 @@ if 'KEY' not in dir(conf):
             KeyId=conf.KMS_KEY_ID,
             Plaintext=key)
         encoded = response['CiphertextBlob'].encode('base64')
-        print('')
-        print('Add the following to ' + CONF_FILE_NAME + ' -----------')
-        print('')
-        print('KEY = """' + encoded + '"""')
-        print('')
+        say('')
+        say('Add the following to ' + CONF_FILE_NAME + ' -----------')
+        say('')
+        say('KEY = """' + encoded + '"""')
+        say('')
     else:
         exit(1)
 elif conf.KMS_KEY_ID is None:
-    print('KMS key id not set in ' + CONF_FILE_NAME)
+    say('KMS key id not set in ' + CONF_FILE_NAME)
     exit(1)
 
 fernet_key = client.decrypt(
@@ -44,7 +57,7 @@ fernet_key = client.decrypt(
 fernet = Fernet(fernet_key.encode('ascii'))
 
 if len(sys.argv) < 2:
-    print('pass decrypt or encrypt as first argument')
+    say('pass decrypt or encrypt as first argument')
     exit(1)
 
 
@@ -54,29 +67,51 @@ def decrypt(filename):
         decrypted = fernet.decrypt(contents.decode('base64'))
     return decrypted
 
+
+def commit_encrypted_files(file_names):
+    if file_names:
+        say('The following secret files were changed:')
+        for file_name in file_names:
+            say('\t' + file_name)
+        say('Please enter a commit message for your change:')
+        commit_message = raw_input()
+        say('committing')
+        cmd = 'git commit %s -am "%s"' % (' '.join(file_names), commit_message)
+        say(cmd)
+        subprocess.call(cmd)
+
+encrypted_file_names = []
 if sys.argv[1] == 'encrypt':
-    for filename in conf.FILES_TO_ENCRYPT:
-        if not os.path.isfile(filename):
-            print('secrets2git: ' + filename + ' does not exist, skipping')
+    for file_name in conf.FILES_TO_ENCRYPT:
+        if not os.path.isfile(file_name):
+            say(file_name + ' does not exist, skipping')
             continue
-        with open(PARENT_DIR + filename, 'rb') as in_file:
+        with open(PARENT_DIR + file_name, 'rb') as in_file:
             contents = in_file.read()
-            if contents != decrypt(filename):
+            if contents != decrypt(file_name):
                 header = 'Encrypted with secrets2git'.ljust(76, '-') + '\n'
                 encrypted = header + fernet.encrypt(contents).encode('base64')
-                print('secrets2git:' + filename + ' changed, encrypting...')
-                with open(PARENT_DIR + filename + EXTENSION, 'w') as out_file:
+                say('Encrypting ' + file_name)
+                with open(PARENT_DIR + file_name + EXTENSION, 'w') as out_file:
                     out_file.write(encrypted)
+                encrypted_file_names.append(file_name + EXTENSION)
+    if not encrypted_file_names:
+        say('No secrets changed')
+    if os.environ.get('SECRETS2GIT_COMMIT', None):
+        commit_encrypted_files(encrypted_file_names)
+    elif encrypted_file_names:
+        say('Please commit:')
+        for file_name in encrypted_file_names:
+            say('\t' + file_name)
 elif sys.argv[1] == 'decrypt':
-    for filename in conf.FILES_TO_ENCRYPT:
-        print('secrets2git: decrypting ' + filename)
-        decrypted = decrypt(filename)
-        with open(PARENT_DIR + filename, 'w') as out_file:
+    for file_name in conf.FILES_TO_ENCRYPT:
+        say('decrypting ' + file_name)
+        decrypted = decrypt(file_name)
+        with open(PARENT_DIR + file_name, 'w') as out_file:
             out_file.write(decrypted)
 else:
-    print('invalid first argument, must be decrypt or encrypt')
+    say('invalid first argument, must be decrypt or encrypt')
 
-print('secrets2git: finished')
 # TODO: Git submodule update --init
 # TODO: Tries to install secrets2git dependencies
 # TODO: '' decrypts this file and stores it here on git pull - in dev and cloud
