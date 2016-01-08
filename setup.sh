944bc880519286b885ac963baecdbaac2802af28
crizCraig <cquiter@gmail.com>
2016-01-08 13:25:15 -0800

    Add `submodule update --init` and initial decryption to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -e
 
+# Verified on Mac OSX Yosemite and El Capitan
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Go to parent project
+cd ${DIR}
+cd ..
+
+# Get secrets2git submodule
+git submodule update --init
+
 # Clarify sudo password prompt
 sudo -p "System password for sudo commands: " echo 1 >/dev/null 2>&1;
 
@@ -10,8 +21,9 @@ command -v pip >/dev/null 2>&1 || {
     sudo -H easy_install pip;
 }
 
+# Install cryptography package
 if ! hash openssl1 2>/dev/null && [[ "$OSTYPE" == "darwin"* ]]; then
-    # Make sure openssl is installed (for El Capitan)
+  # Make sure openssl is installed (for El Capitan)
     brew install openssl 2> /dev/null || true;
     sudo -H env LDFLAGS="-L$(brew --prefix openssl)/lib" CFLAGS="-I$(brew --prefix openssl)/include" pip install -I cryptography
 else
@@ -19,3 +31,6 @@ else
 fi
 
 sudo -H pip install --ignore-installed six -r requirements.txt
+
+# Do initial decryption
+python secrets2git/secrets2git.py decrypt
