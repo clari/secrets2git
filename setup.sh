fc380594fd9d295b94b736bad79df33a61c9d688
Craig Quiter <cquiter@gmail.com>
2016-08-19 22:15:43 -0700

    fix initial decrypt path check

--- setup.sh
+++ setup.sh
@@ -41,7 +41,7 @@ fi
 
 sudo -H pip install --ignore-installed six -r requirements.txt
 
-if [ -f ${DIR}../Secrets2GitConf.py ]; then
+if [ -f ${DIR}/../Secrets2GitConf.py ]; then
   # If existing project, do initial decryption
   python secrets2git.py decrypt
 fi
